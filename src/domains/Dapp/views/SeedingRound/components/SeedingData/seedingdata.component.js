b1391a38713e24953a610044efada6e95c9abc21
Azarius <as.haerens@gmail.com>
2021-03-04 16:06:01 +0100

    added SeedingData component

---
+++ src/domains/Dapp/views/SeedingRound/components/SeedingData/seedingdata.component.js
@@ -0,0 +1,63 @@
+import { Fragment, useEffect, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import useSWR from 'swr';
+import { Card, Countdown, Progress, Button, Input, Flexbox, TextSmall } from '@core/components';
+import { Section, LabeledCard, Grid } from '@dapp/components';
+import { ABI_SEED, CONTRACT_ADDRESS } from '@constants/index';
+import { fetcher } from '@utils/index';
+
+const SeedingData = () => {
+
+    const { library, account } = useWeb3React();
+	const [ purchaseInputValue, setPurchaseInputValue ] = useState(null);
+	const onChangeInputPurchase = (value) => {
+		setPurchaseInputValue(value);
+	};
+
+	const { data: priceAtLaunch, mutate: getPriceAtLaunch } = useSWR([ CONTRACT_ADDRESS.seed, 'priceAtLaunch' ], {
+		fetcher: fetcher(library, ABI_SEED)
+	});
+
+	useEffect(() => {
+		library.on('block', () => {
+			getPriceAtLaunch(undefined, true);
+		});
+		return () => {
+			library && library.removeAllListeners('block');
+		};
+	},[ library, getPriceAtLaunch ]);
+	
+	console.log(priceAtLaunch);
+
+    return (
+		<Fragment>
+			<Section label="general info">
+				<Flexbox gap="60px">
+					<LabeledCard label="time remaining" gutter={40}>
+						<Countdown endDate="2021-04-20" />
+					</LabeledCard>
+					<LabeledCard label="total funds collected" gutter={40}>
+						<Progress currentValue={513} totalValue={2000} label="bnb" />
+					</LabeledCard>
+				</Flexbox>
+			</Section>
+			<Section label="personal data">
+				<Grid>
+					<LabeledCard label="wallet data" gutter={40} />
+					<LabeledCard label="purchase funds" gutter={40}>
+						<Flexbox gap="20px">
+							<TextSmall>{purchaseInputValue * 0.3} UWU</TextSmall>
+							<Flexbox gap="15px" direction="horizontal">
+								<Input onChange={onChangeInputPurchase} />
+								<Button>purchase</Button>
+							</Flexbox>
+						</Flexbox>
+					</LabeledCard>
+				</Grid>
+			</Section>
+		</Fragment>
+	);
+
+};
+
+export default SeedingData;
--- src/domains/Dapp/views/SeedingRound/seedinground.component.js
+++ src/domains/Dapp/views/SeedingRound/seedinground.component.js
@@ -1,62 +1,26 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment } from 'react';
 import { useWeb3React } from '@web3-react/core';
-import useSWR from 'swr';
-import { Card, Countdown, Progress, Button, Input, Flexbox, TextSmall } from '@core/components';
-import { Section, LabeledCard, Grid } from '@dapp/components';
-import { ABI_SEED, CONTRACT_ADDRESS } from '@constants/index';
-import { fetcher } from '@utils/index';
+import { Section, DisconnectedWalletCard } from '@dapp/components';
+import { SeedingData } from './components';
 
 const SeedingRound = () => {
 	
-	const { library, account } = useWeb3React();
-	const [ purchaseInputValue, setPurchaseInputValue ] = useState(null);
-	const onChangeInputPurchase = (value) => {
-		setPurchaseInputValue(value);
+	const { active } = useWeb3React();
+
+	const renderSeedingData = () => {
+		if (!active) {
+			return (
+				<Fragment>
+					<Section>
+						<DisconnectedWalletCard />
+					</Section>
+				</Fragment>
+			);
+		}
+		return <SeedingData />
 	};
 
-	const { data: priceAtLaunch, mutate: getPriceAtLaunch } = useSWR([ CONTRACT_ADDRESS.seed, 'priceAtLaunch' ], {
-		fetcher: fetcher(library, ABI_SEED)
-	});
-
-	useEffect(() => {
-		library && library.on('block', () => {
-			getPriceAtLaunch && getPriceAtLaunch(undefined, true);
-		});
-		return () => {
-			library && library.removeAllListeners('block');
-		};
-	},[ library, getPriceAtLaunch ]);
-	
-	console.log(priceAtLaunch);
-
-	return (
-		<Fragment>
-			<Section label="general info">
-				<Flexbox gap="60px">
-					<LabeledCard label="time remaining" gutter={40}>
-						<Countdown endDate="2021-04-20" />
-					</LabeledCard>
-					<LabeledCard label="total funds collected" gutter={40}>
-						<Progress currentValue={513} totalValue={2000} label="bnb" />
-					</LabeledCard>
-				</Flexbox>
-			</Section>
-			<Section label="personal data">
-				<Grid>
-					<LabeledCard label="wallet data" gutter={40} />
-					<LabeledCard label="purchase funds" gutter={40}>
-						<Flexbox gap="20px">
-							<TextSmall>{purchaseInputValue * 0.3} UWU</TextSmall>
-							<Flexbox gap="15px" direction="horizontal">
-								<Input onChange={onChangeInputPurchase} />
-								<Button>purchase</Button>
-							</Flexbox>
-						</Flexbox>
-					</LabeledCard>
-				</Grid>
-			</Section>
-		</Fragment>
-	);
+	return renderSeedingData();
 };
 
 export default SeedingRound;
