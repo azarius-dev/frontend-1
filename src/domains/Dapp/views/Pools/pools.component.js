9410a5b3264848ddb5f0e94935fca3ad3b93a6b0
PunkUnknown <PunkUnknown@tutanota.com>
2021-03-21 13:28:39 +0500

    pool info text

--- src/domains/Dapp/views/Pools/pools.component.js
+++ src/domains/Dapp/views/Pools/pools.component.js
@@ -13,19 +13,40 @@ const Pools = () => {
 		if (!active) return <DisconnectedWalletCard />;
 		return (
 			<Grid>
-				<PoolCard label="DEBASE Bridge Pool" info="**update**" routePath="/pools/debase-bridge-pool" isActive>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun
+				<PoolCard label="DEBASE Bridge Pool" routePath="/pools/debase-bridge-pool" isActive>
+					Pool that bridges DEBASE deposits you have made on the DEBASE bridge on Ethereum. To allow you to
+					mine UwU in return.
+					<br />
+					<br />
+					This pool will give out a total of 10000 UwU over its life time. With initially giving out 5000 UwU
+					over a period of 3.5 days. After which the given will half to 2500 UwU given again over a period of
+					another 3.5 days.
+					<br />
+					<br />
+					This reward halving process will until the 10000 UwU are distributed.
 				</PoolCard>
-				<PoolCard
-					label="DEBASE/DAI LP Bridge Pool"
-					info="**update**"
-					routePath="/pools/debase-eth-lp-bridge-pool"
-					isActive
-				>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun
+				<PoolCard label="DEBASE/DAI LP Bridge Pool" routePath="/pools/debase-eth-lp-bridge-pool" isActive>
+					Pool that bridges DEBASE/DAI LP deposits you have made on the DEBASE/DAI LP bridge on Ethereum. To
+					allow you to mine UwU in return.
+					<br />
+					<br />
+					This pool will give out a total of 15000 UwU over its life time. With initially giving out 7500 UwU
+					over a period of 3.5 days. After which the given will half to 3750 UwU given again over a period of
+					another 3.5 days.
+					<br />
+					<br />
+					This reward halving process will until the 15000 UwU are distributed.
 				</PoolCard>
-				<PoolCard label="UwU/BUSD LP Pool" info="**update**" routePath="/pools/uwu-busd-lp-pool" isActive>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun
+				<PoolCard label="UwU/BUSD LP Pool" routePath="/pools/uwu-busd-lp-pool" isActive>
+					Pool accepts UWU/BUSD lP deposits and in return allows you to mine UwU.
+					<br />
+					<br />
+					This pool will give out a total of 700000 UwU over its life time. With initially giving out 350000
+					UwU over a period of 3.5 days. After which the given will half to 175000 UwU given again over a
+					period of another 3.5 days.
+					<br />
+					<br />
+					This reward halving process will until the 700000 UwU are distributed.
 				</PoolCard>
 			</Grid>
 		);
